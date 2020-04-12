925c32361579c4e0219593fce8767485aefdfc14
Abhishek Kundalia <abhishek@zestmoney.in>
2020-04-12 09:29:23 +0000

    created new deck

--- main.go
+++ main.go
@@ -1,14 +1,6 @@
 package main
 
 func main() {
-	// card := "Ace of Spades"
-	// card = newCard()
-	cards := deck{"Ace of diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	//fmt.Println(cards)
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of diamonds"
 }
